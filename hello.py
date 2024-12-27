0c75856a47a4e376d5a6911c89b05b09baed6a2d
pgdai19 <pgdai19@gmail.com>
2024-12-27 19:18:04 +0530

    8pm Commit

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Now it is Lunch time"
+    return "Leave Us Sir, Its Already 8 PM"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
